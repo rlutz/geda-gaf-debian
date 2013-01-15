40e20106a6eb320c5a197723c5aba0547c9bcd68
Bdale Garbee <bdale@gag.com>
2013-01-15 09:56:50 -0700

    Imported Upstream version 1.8.1

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 /* Date part of package version string. */
-#define PACKAGE_DATE_VERSION "20121118"
+#define PACKAGE_DATE_VERSION "20121123"
 
 /* Dotted part of package version string. */
-#define PACKAGE_DOTTED_VERSION "1.8.0"
+#define PACKAGE_DOTTED_VERSION "1.8.1"
 
 /* Current git commit. */
-#define PACKAGE_GIT_COMMIT "bbb663612afa98c274279edbe167f86dd7b3f6d8"
+#define PACKAGE_GIT_COMMIT "d088ec192ac9fbc1dff3509527cb238520c2ee33"
 
 /* Current 'git describe' version string. */
-#define PACKAGE_GIT_VERSION "1.8.0-20121118"
+#define PACKAGE_GIT_VERSION "1.8.1-20121123"
